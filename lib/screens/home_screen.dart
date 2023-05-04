7a67407311e533edc3ee86ba1001635f6c45b54d
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-04 16:15:03 +0100

    ...

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+class HomeScreen
+ extends StatefulWidget {
+  const HomeScreen
+  ({super.key});
+
+  @override
+  State<HomeScreen
+  > createState() => _HomeScreen
+  State();
+}
+
+class _HomeScreen
+State extends State<HomeScreen
+> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
