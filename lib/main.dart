ce5b04ac99c3c42c583e51d30db2015e615df745
IBEABUCHI EMMANUEL IHEKE (Sever) <developersever@gmail.com>
2023-05-04 16:16:32 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'PDF READER',
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home:  HomeScreen(),
     );
   }
 }
